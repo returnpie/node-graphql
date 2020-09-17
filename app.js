808fe1bbef32a1da79dd65545f2af107ee16327c
pie <returnpie@icloud.com>
2020-09-18 00:07:49 +0900

    add user model, auth, validating, transform async / await

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -48,17 +49,24 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
   .connect(
-    "mongodb+srv://returnpie:MBeq4M910DIKfizJ@cluster0.izand.mongodb.net/post-app"
+    "mongodb+srv://returnpie:MBeq4M910DIKfizJ@cluster0.izand.mongodb.net/post-app",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
   )
   .then((result) => {
     app.listen(8080, () => {
--- routes/feed.js
+++ routes/feed.js
@@ -3,13 +3,15 @@ const express = require("express");
 const { body } = require("express-validator");
 
 const feedController = require("../controller/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
   "/post",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -17,10 +19,11 @@ router.post(
   feedController.createPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -28,6 +31,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;

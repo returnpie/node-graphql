63e197e443c7e897e02da30edc48c7b99a38923f
pie <returnpie@icloud.com>
2020-09-18 06:35:42 +0900

    add create, update, delete post with socket.io

--- app.js
+++ app.js
@@ -69,8 +69,12 @@ mongoose
     }
   )
   .then((result) => {
-    app.listen(8080, () => {
+    const server = app.listen(8080, () => {
       console.log(`server listening on 8080`);
+    });
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
     });
   })
   .catch((err) => console.log(err));
